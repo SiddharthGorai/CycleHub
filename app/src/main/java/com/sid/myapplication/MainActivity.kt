10b63a22d8bee50ccb379789ada065d5b138cd41
SiddharthGorai <112846822+SiddharthGorai@users.noreply.github.com>
2023-05-25 10:17:20 +0530

    Recyclerview

--- app/src/main/java/com/sid/myapplication/MainActivity.kt
+++ app/src/main/java/com/sid/myapplication/MainActivity.kt
@@ -1,9 +1,24 @@
 package com.sid.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.net.toUri
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
@@ -12,23 +27,140 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.util.Objects
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var database: FirebaseDatabase
+    private lateinit var signoutAlert: AlertDialog.Builder
     private lateinit var auth: FirebaseAuth
     private lateinit var myAdapter: MyAdapter
     private lateinit var recyclerView: RecyclerView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var alertDialog: AlertDialog.Builder
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else {
+            return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun changeMenuBar(menu: Menu?) {
+        val auth = Firebase.auth
+        val userID = auth.currentUser!!.uid
+        database = Firebase.database
+        val myRef = database.reference
+        myRef.child("Admins").child(userID).get().addOnSuccessListener {
+            val uName = it.child("adminName").value.toString()
+            val uEmail = it.child("adminEmail").value.toString()
+
+            val dName = menu?.findItem(R.id.dname)
+            val dEmail = menu?.findItem(R.id.demail)
+            dEmail?.title = uEmail
+            dName?.title = uName
 
 
+        }.addOnFailureListener {
+            Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun doThis() {
+        signoutAlert = AlertDialog.Builder(this)
+        signoutAlert.setTitle("Alert!")
+            .setMessage("Do you want to Sign out ?")
+            .setCancelable(true)
+            .setPositiveButton("Yes") { dialogInterface, it ->
+                Firebase.auth.signOut()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            .setNegativeButton("No") { dialogInterface, it ->
+                dialogInterface.cancel()
+            }.show()
+
+    }
+
+    private fun changePassword(menu: Menu) {
+
+        val uBar = findViewById<ProgressBar>(R.id.uBar)
+        uBar.visibility = View.VISIBLE
+
+        val auth = Firebase.auth
+        val dEmail = menu.findItem(R.id.demail)
+        val uEmail = dEmail.title.toString()
+
+        auth.sendPasswordResetEmail(uEmail.trim())
+            .addOnSuccessListener {
+                uBar.visibility = View.INVISIBLE
+                val alertDialog = AlertDialog.Builder(this)
+                alertDialog.setTitle("CycleHub")
+                    .setCancelable(true)
+                    .setMessage("A password change link has been sent to your eamil.")
+                    .setNegativeButton("Ok") { dialogInterface, it ->
+                        dialogInterface.cancel()
+                    }.show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+
+    }
+    @SuppressLint("MissingInflatedId")
+    fun showCycle(cycleNameA:String, cycleDescA:String, cycleUrlA:String){
+        val inflater = layoutInflater
+        val view: View = inflater.inflate(R.layout.cycle_popup,null)
+        view.findViewById<TextView>(R.id.heading).text = cycleNameA
+        view.findViewById<TextView>(R.id.description).text = cycleDescA
+        Glide.with(this@MainActivity).load(cycleUrlA)
+            .into(view.findViewById(R.id.cycleImageA)
+            )
+
+        alertDialog = AlertDialog.Builder(this)
+        alertDialog.setView(view)
+            .setCancelable(true)
+            .show()
+
+    }
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        drawerLayout = findViewById(R.id.my_drawer_layout)
+        actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout, R.string.nav_open, R.string.nav_close
+        )
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        // to make the Navigation drawer icon always appear on the action bar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navView: NavigationView = findViewById(R.id.navView)
+        val menu: Menu = navView.menu
+        changeMenuBar(menu)
+        navView.setNavigationItemSelectedListener { it ->
+            when (it.itemId) {
+                R.id.signOut -> doThis()
+                R.id.changePass -> changePassword(menu)
+            }
+            true
+        }
+
         auth = Firebase.auth
         database = Firebase.database
         val userID = auth.currentUser!!.uid
-        val myRef = database.getReference("Data").child(userID)
+        val myRef = database.getReference("Data")
 
         val cycleList: ArrayList<cycleData> = ArrayList()
 
@@ -37,14 +169,9 @@ class MainActivity : AppCompatActivity() {
                 if (snapshot.exists()) {
                     for (i in snapshot.children) {
                         // i = 1,2,3..
-                        var cycleName: String = ""
-                        var cycleDesc: String = ""
-                        var cycleUrl: String = ""
-                        for (j in i.children) {
-                            if (j.key.toString() == "CycleName") cycleName = j.value.toString()
-                            if (j.key.toString() == "CycleDesc") cycleDesc = j.value.toString()
-                            if (j.key.toString() == "CycleUrl") cycleUrl = j.value.toString()
-                        }
+                        val cycleName = i.child("CycleName").value.toString()
+                        val cycleDesc = i.child("CycleDesc").value.toString()
+                        val cycleUrl = i.child("CycleUrl").value.toString()
                         if (!cycleList.contains(cycleData(cycleName, cycleDesc, cycleUrl))) {
                             cycleList.add(cycleData(cycleName, cycleDesc, cycleUrl))
                         }
@@ -53,8 +180,24 @@ class MainActivity : AppCompatActivity() {
                     val layoutManager = LinearLayoutManager(this@MainActivity)
                     recyclerView = findViewById(R.id.RecyclerView)
                     recyclerView.layoutManager = layoutManager
-                    myAdapter = MyAdapter(cycleList,this@MainActivity)
+                    myAdapter = MyAdapter(cycleList, this@MainActivity)
                     recyclerView.adapter = myAdapter
+
+                    myAdapter.setOnItemClickListener(object : MyAdapter.onItemClickListener {
+                        override fun onItemClick(position: Int) {
+                            val cycleNameA = recyclerView.findViewHolderForAdapterPosition(position)?.
+                            itemView?.findViewById<TextView>(R.id.cycleName)?.text.toString()
+
+                            val cycleDescA = recyclerView.findViewHolderForAdapterPosition(position)?.
+                            itemView?.findViewById<TextView>(R.id.cycleDesc)?.text.toString()
+
+                            val cycleUrlA = recyclerView.findViewHolderForAdapterPosition(position)?.
+                            itemView?.findViewById<TextView>(R.id.url)?.text.toString()
+
+                            showCycle(cycleNameA,cycleDescA,cycleUrlA)
+                        }
+                    })
+
 
                 }
             }
@@ -63,6 +206,7 @@ class MainActivity : AppCompatActivity() {
                 TODO("Not yet implemented")
             }
         })
+
 
     }
 }
--- app/src/main/java/com/sid/myapplication/MyAdapter.kt
+++ app/src/main/java/com/sid/myapplication/MyAdapter.kt
@@ -14,11 +14,22 @@ class MyAdapter(
     private val context: MainActivity
 ) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
+    private lateinit var mListener: onItemClickListener
+
+    interface onItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: onItemClickListener) {
+        mListener = listener
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val itemView =
             LayoutInflater.from(parent.context).inflate(R.layout.cycle_list, parent, false)
-        return MyViewHolder(itemView)
+        return MyViewHolder(itemView, mListener)
+
+
     }
 
     override fun getItemCount(): Int {
@@ -29,12 +40,21 @@ class MyAdapter(
         Glide.with(context).load(cycleList[position].cycleUrl).into(holder.cycleImg)
         holder.cycleName.text = cycleList[position].cycleName
         holder.cycleDesc.text = cycleList[position].cycleDesc
+        holder.Url.text = cycleList[position].cycleUrl
 
     }
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class MyViewHolder(itemView: View, listener: onItemClickListener) :
+        RecyclerView.ViewHolder(itemView) {
         val cycleName: TextView = itemView.findViewById(R.id.cycleName)
         val cycleDesc: TextView = itemView.findViewById(R.id.cycleDesc)
         val cycleImg: ImageView = itemView.findViewById(R.id.cycleImg)
+        val Url: TextView = itemView.findViewById(R.id.url)
+
+        init {
+            itemView.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+            }
+        }
     }
 }
